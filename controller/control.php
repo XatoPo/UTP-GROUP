21d62fb0a72f6f1a22cf39f617cb351d872240e8
SebaSalvador <sebasalvadorqt@gmail.com>
2024-06-08 14:37:37 -0500

    Se agrega funcion en control y se elimina control_profile

--- controller/control.php
+++ controller/control.php
@@ -5,6 +5,8 @@ error_reporting(E_ALL);
 session_start();
 
 include "../util/connection.php";
+include '../dao/utp_group_dao.php';
+
 $action = isset($_GET["opc"]) ? $_GET["opc"] : "";
 
 switch ($action) {
@@ -64,6 +66,34 @@ function iniciarSesion($conn) {
             mysqli_stmt_close($stmt);
         }
     }
+}
+
+function getProfileDataByID($student_id) {
+    // Obtener los datos del estudiante llamando a la función en el DAO
+    $student_data = getStudentDataById($student_id);
+    $hobbies_data = getStudentHobbiesById($student_id);
+    $softskills_data = getStudentSkillsById($student_id,'Skills Blandas');
+    $hardskills_data = getStudentSkillsById($student_id,'Skills Técnicas');
+    
+    if ($student_data) {
+        $profile_data = [
+            'student_data' => $student_data,
+            'hobbies_data' => $hobbies_data,
+            'softskills_data' => $softskills_data,
+            'hardskills_data' => $hardskills_data
+        ];
+        return json_encode($profile_data);
+    } else {
+        return json_encode(['error' => 'No se encontraron datos para el estudiante con ID: ' . $student_id]);
+    }
+}
+
+// Verificar si se ha proporcionado una ID de estudiante a través de la URL
+if (isset($_GET['id'])) {
+    $student_id = $_GET['id'];
+    echo getProfileDataByID($student_id);
+} else {
+    echo json_encode(['error' => 'No se proporcionó una ID de estudiante']);
 }
 
 ?>
