95cb4ed4cec6c3eca7cbd3e29f6d894f2dffb65e
Eduarx1509 <eduarx150903@gmail.com>
2024-06-08 18:09:36 -0500

    Cambio de estilo en index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -11,42 +11,36 @@
 
 <body class="bg-[#f6f9ff] h-screen flex items-center justify-center">
     <main>
-        <div class="grid grid-cols-3 w-[1200px]">
-            <div class="col-span-2">
-                <img src="images/login/web-login-pao.png" alt="">
+        <div class="grid grid-cols-2 w-[1200px] bg-white rounded-xl overflow-hidden">
+            <div class="bg-[#f6f9ff] flex items-center justify-center">
+                <img src="images/login/web-login-pao.png" alt="Illustration" class="h-full object-cover">
             </div>
-            <div class="col-span-1 flex flex-col items-center justify-center gap-y-5">
-                <div class="flex flex-col items-center gap-y-1">
-                    <h1 class="text-2xl font-bold text-gray-700">Bienvenido a</h1>
-                    <img src="images/logo/logo-pao-class.png" class="w-56" alt="">
+            <div class="flex flex-col items-center justify-center p-10 gap-y-5">
+                <div class="flex flex-col items-center gap-y-1 mb-5">
+                    <img src="images/logo/logo-pao-class.png" class="w-56" alt="UTP + class">
                 </div>
-<<<<<<< HEAD
-
-                <h3 class="font-bold text-3xl">INICIO DE SESIÓN</h3>
-                
-                <form action="controller/control.php?opc=1" method="POST">
-                    <div class="flex flex-col relative">
+                <h3 class="font-bold text-3xl mb-5 text-gray-700">La nueva experiencia digital de aprendizaje</h3>
+                <form action="controller/control.php?opc=1" method="POST" class="w-full max-w-xs">
+                    <div class="flex flex-col relative mb-5">
                         <input
-                            class="peer bg-[#f6f9ff] rounded-md border-2 p-2 focus:outline-none placeholder-transparent focus:border-black border-gray-400 w-[300px]"
+                            class="peer bg-[#f6f9ff] rounded-md border-2 p-2 focus:outline-none placeholder-transparent focus:border-black border-gray-400"
                             type="text" name="id" id="id" placeholder="Correo electrónico" required>
                         <label
-                            class="absolute bg-[#f6f9ff] inline-flex px-2 ms-2 -top-3 text-sm text-black transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-black peer-focus:text-sm peer-focus:px-2"
-                            for="id">Código de estudiante</label>
-
+                            class="absolute bg-[#f6f9ff] inline-flex px-2 left-2 -top-3 text-sm text-black transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-black peer-focus:text-sm peer-focus:px-2"
+                            for="id">Código UTP</label>
                     </div>
-                    <div class="flex flex-col relative">
+                    <div class="flex flex-col relative mb-5">
                         <input
-                            class="peer bg-[#f6f9ff] rounded-md border-2 p-2 focus:outline-none placeholder-transparent focus:border-black border-gray-400 w-[300px]"
+                            class="peer bg-[#f6f9ff] rounded-md border-2 p-2 focus:outline-none placeholder-transparent focus:border-black border-gray-400"
                             type="password" name="password" id="password" placeholder="Contraseña" required>
                         <label
-                            class="absolute bg-[#f6f9ff] inline-flex px-2 ms-2 -top-3 text-sm text-black  transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-black  peer-focus:text-sm peer-focus:px-2"
+                            class="absolute bg-[#f6f9ff] inline-flex px-2 left-2 -top-3 text-sm text-black transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-black peer-focus:text-sm peer-focus:px-2"
                             for="password">Contraseña</label>
                     </div>
-                    <a class="text-blue-600 hover:underline" href="#">¿Olvidaste tu contraseña?</a>
-                    <input type="submit" value="Iniciar Sesión" name="iniciar_sesion"
-                        class="bg-black w-[300px] py-2 rounded-lg text-white border border-black hover:bg-white hover:text-black transition">
+                    <a class="text-blue-600 hover:underline mb-5 block text-right" href="#">Restablecer contraseña</a>
+                    <input type="submit" value="Iniciar sesión" name="iniciar_sesion"
+                        class="bg-blue-600 w-full py-2 rounded-lg text-white border border-blue-600 hover:bg-white hover:text-blue-600 transition">
                 </form>
-                
             </div>
         </div>
     </main>
