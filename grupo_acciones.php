5bb26318df5d33f34f2ba88c3f4b3a3c4fadcb25
XatoPo <flaviovm2013@gmail.com>
2024-06-09 18:17:44 -0500

    ACCIONES

--- grupo_acciones.php
+++ grupo_acciones.php
@@ -4,27 +4,21 @@ include_once 'util/connection.php';
 
 session_start();
 
-if (!isset($_SESSION['student_id'])) {
-    echo json_encode(['error' => 'Unauthorized']);
-    exit();
-}
+if (isset($_POST['action']) && isset($_POST['group_id']) && isset($_POST['student_id'])) {
+    $action = $_POST['action'];
+    $group_id = $_POST['group_id'];
+    $student_id = $_POST['student_id'];
 
-$student_id = $_SESSION['student_id'];
-$action = $_POST['action'];
-$group_id = $_POST['group_id'];
+    $obj = new utp_group_dao();
 
-$obj = new utp_group_dao();
-
-if ($action === 'unirse') {
-    $students_in_group = $obj->ObtenerAlumnosPorGrupoId($group_id);
-    if (count($students_in_group) < $group_info['number_of_students']) {
+    if ($action === 'unirse') {
         $obj->AgregarAlumnoEnGrupo($group_id, $student_id);
-        echo json_encode(['success' => true]);
-    } else {
-        echo json_encode(['error' => 'Group is full']);
+    } elseif ($action === 'salir') {
+        $obj->EliminarAlumnoDelGrupo($group_id, $student_id);
     }
-} elseif ($action === 'salir') {
-    $obj->EliminarAlumnoDelGrupo($group_id, $student_id);
-    echo json_encode(['success' => true]);
+
+    echo 'success';
+} else {
+    echo 'error';
 }
 ?>
