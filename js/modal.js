3ad302c19c61776cabfd2b26831b4db51a7d2700
Cheskito <francescorivar@gmail.com>
2024-06-09 10:14:35 -0500

    uwu

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,5 @@
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    var closeModalBtn = document.getElementById('closeModalBtn');
-    var modal = document.getElementById('myModal'+idg);
-
-    closeModalBtn.addEventListener('click', () => {
-        modal.classList.add('hidden');
-        modal.classList.remove('flex');
-    });
-
-    window.addEventListener('click', (event) => {
-        if (event.target == modal) {
-            modal.classList.add('hidden');
-            modal.classList.remove('flex');
-        }
-    });
-});
-
 function openModal(number) {
     const modal = document.getElementById('myModal');
     const modalTitle = document.getElementById('modal-title');
@@ -26,14 +8,8 @@ function openModal(number) {
     modal.classList.add('flex');
     modalTitle.innerHTML = 'Confirmación de entrada al grupo ' + number;
     modalContent.innerHTML = '¿Deseas entrar al grupo ' + number + '?';
-}
 
-function openProfile(id) {
-    const modal = document.getElementById('myModal'+id);
-    modal.classList.remove('hidden');
-    modal.classList.add('flex');
-
-    var closeModalBtn = document.getElementById('closeModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
 
     closeModalBtn.addEventListener('click', () => {
         modal.classList.add('hidden');
@@ -48,4 +24,22 @@ function openProfile(id) {
     });
 }
 
-var idg;
+function openProfile(id) {
+    const modal = document.getElementById('myModal'+id);
+    modal.classList.remove('hidden');
+    modal.classList.add('flex');
+
+    const closeModalBtn = document.getElementById('closeModalBtn');
+
+    closeModalBtn.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        modal.classList.remove('flex');
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.classList.add('hidden');
+            modal.classList.remove('flex');
+        }
+    });
+}
